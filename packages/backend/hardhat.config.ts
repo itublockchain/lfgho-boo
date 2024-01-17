57511d0c16cee838c6b17277e9f3aa022eddbb61
koybasimuhittin <koybasimuhittin@gmail.com>
2024-01-17 18:55:57 +0300

    update solidity version

--- packages/backend/hardhat.config.ts
+++ packages/backend/hardhat.config.ts
@@ -1,8 +1,10 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-};
+	solidity: {
+		compilers: [{ version: "0.8.16" }, { version: "0.6.6" }],
+	},
+}
 
-export default config;
+export default config
