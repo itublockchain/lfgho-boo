64567f039bb464e5d5af381e61d98484a95fb4ed
Emirhan-Cavusoglu-sftw <emirhancavusoglu.sftw@gmail.xom>
2024-01-20 05:19:25 +0300

    Home Page  Design

--- packages/frontend/my-app/app/components/deposit-button.tsx
+++ packages/frontend/my-app/app/components/deposit-button.tsx
@@ -5,7 +5,8 @@ const DepositButton = () => {
   const [open, setOpen] = useState(false);
     return (
         <div>
-      <button className="bg-black px-4 py-2 rounded text-white"
+      <button className="bg-[#99DDE2] text-black font-sans  font-bold border-black border-[3px]
+          border-solid px-6 py-1 rounded-2xl textcolor-white"
       onClick={() => setOpen(true)}>
         <a href="/deposit">Deposit</a>
       </button></div>
--- packages/frontend/my-app/app/page.tsx
+++ packages/frontend/my-app/app/page.tsx
@@ -1,59 +1,88 @@
-'use client';
+"use client";
 import { useModal } from "connectkit";
 import { useAccount, useDisconnect } from "wagmi";
 import React from "react";
 import { ConnectButton } from "orangekit";
-import Link from 'next/link';
+import Link from "next/link";
 import { linea } from "viem/chains";
 import DepositButton from "./components/deposit-button";
 import PaybackButton from "./components/payback-button";
-
+import Image from "next/image";
 
 export default function Home() {
   const { isConnected, address, isConnecting } = useAccount();
   const { setOpen } = useModal();
   const { disconnect } = useDisconnect();
+  
+  
+  
+  
+  
+  
   if (isConnecting) return <p>Connecting...</p>;
   return (
-    <div id="anasayfa" className="p=20     " >
+    <div id="anasayfa" className="p=20 flex justify-between    ">
+      <>
+        <div className="flex   space-x-8 justify-center items-center  ">
+          <Image
+            src={"/boo.png"}
+            alt=""
+            width={100}
+            height={50}
+            className="pb-10"
+          ></Image>
+          <DepositButton />
+          <PaybackButton />
+        </div>
 
-      {
-        !isConnected && (
-          <button className="bg-black text-white p-2 rounded-md textcolor-white"
-            onClick={() => setOpen(true)}>
-            Connect Wallet
-          </button>
-        )
-      }
-      {
-        isConnected && (
-          <div>
-            <p>Connected Wallet: {address}</p>
-            <button onClick={() => disconnect()}>
-              Disconnect
+        <div className="flex space-x-5  mt-10 mr-8">
+          {!isConnected && (
+            <button
+              className="bg-[#99DDE2] text-black font-sans font-bold border-black border-[3px]
+          border-solid border-l-8 border-b-8  rounded-md textcolor-white px-5 py-1 h-11
+          "
+              onClick={() => setOpen(true)}
+            >
+              Connect Wallet
+            </button>
+          )}
+          {isConnected && (
+            <div>
+              <p>Connected Wallet: {address}</p>
+              <button onClick={() => disconnect()}>Disconnect</button>
+            </div>
+          )}
+
+          <div
+            className="absolute top-[40%] left-[50%] translate-y-[-50%] translate-x-[-50%] text-sm/[24px] font-bold
+       bg-[#8AF3FB] p-30 rounded-[10px] shadow-[100px_35px_35px_-15px_rgba(0,0,0,0)]  border-l-8 border-b-8 border-black text-center mb-32 
+        w-[500px] h-[300px]  justify-center  flex flex-col items-center   "
+          >
+            <div className="flex mb-14  space-x-8">
+              <div className="flex-col flex">
+                <div className="bg-white border-black border-[3px] rounded-full h-[77px] bg-[url('/BTC_LOGO.jpeg')] bg-center bg-no-repeat bg-cover  "></div>
+                <p className="text-black font-bold font-sans mt-6">Bitcoin</p>
+                <div className=" bg-[#99DDE2] border-[3px] border-black rounded-md mt-3 px-2 py-1">
+                  0,23242
+                </div>
+              </div>
+              <Image src={"/Arrow.png"} alt="" width={170} height={10} className=" h-[16px] mt-6"></Image>
+              <div className="flex-col flex">
+                <div className="bg-white rounded-full border-[3px] border-black border-1 h-[77px] bg-[url('/GHO_LOGO.jpeg')] bg-center bg-no-repeat bg-cover "></div>
+                <p className="text-black font-bold font-sans mt-6">GHO Token</p>
+                <div className=" bg-[#99DDE2] border-[3px] border-black rounded-md mt-3 px-2 py-1">
+                  0,23242
+                </div>
+              </div>
+            </div>
+            <button className="bg-[#b4faff] rounded-md border-[3px] px-4 py-1 mb-4 border-black">
+              Deposit
             </button>
           </div>
-        )
-      }
 
-      <div className="absolute top-[40%] left-[50%] translate-y-[-50%] translate-x-[-50%] text-sm/[24px] font-bold
-       bg-white p-30 rounded-[10px] shadow-[100px_35px_35px_-15px_rgba(0,0,0,0)]  border-l-8 border-b-8 border-black text-center mb-32 
-        w-[500px] h-[300px] ">
-        <h1>Bitcoin Wallet Balance</h1>
-        <p>Bitcoin: 0.236441</p>
-        <p>Deposit: GHO Token 0.233155</p>
-        <p>Bitcoin: 0.236441</p>
-        <p>Deposit: GHO Token 0.233155</p>
-      </div>
-
-      <ConnectButton />
-      <DepositButton />
-      <PaybackButton />
-
-      {/* <Link href="/payback">Payback</Link> */}
-      {/* <Link href="/deposit"> Deposit</Link> */}
-
+          {/* <ConnectButton /> */}
+        </div>
+      </>
     </div>
-  )
+  );
 }
-
